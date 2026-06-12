cdbc67cc23f52f50971ac71eb6db906cb40aa854
rajibkutikasba1 <rajib.kuti.kasba1@gmail.com>
2026-06-13 02:44:24 +0300

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-// অটোমেটিক টেবিল তৈরি করার কোড
-client.query(`
-    CREATE TABLE IF NOT EXISTS patients (
-        id SERIAL PRIMARY KEY,
-        nid VARCHAR(50),
-        name VARCHAR(100),
-        mobile VARCHAR(20)
-    )
-`).then(() => console.log("Table ready!")).catch(err => console.log(err));
